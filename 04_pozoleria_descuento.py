e770e9c048ad7e62f22ea2456e4716e94d900681
jmgarcia005 <74462445+jmgarcia005@users.noreply.github.com>
2020-11-28 10:53:43 -0600

    Tareas

---
+++ 04_pozoleria_descuento.py
@@ -0,0 +1,5 @@
+# Aplicar conocimientos de uso de la funcion map en python
+
+#Agregar un descuento aleatorio de entre 5, 10 y 15 por ciento a todos los precios 
+#Imprimir menu sin descuento e imprimir menu con descuento
+precios_en_menu = [376, 576, 384, 672, 648, 720, 640, 576]
